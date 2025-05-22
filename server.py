baae6d523d5cf0814894409536bc3fc7edfbc50f
Manfredi Piraino <manfredipiraino@gmail.com>
2025-05-22 20:30:38 +0200

    delete: comments

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-print(os.getenv("SECRET_KEY"))
+
 app.secret_key = os.getenv("SECRET_KEY")
 
 def toggle_upload():
